7ab89623c69c282d97f495ea52a3436f9b021d7d
Jesus Guillen <88405002+jesus-guillen02@users.noreply.github.com>
2024-02-05 14:08:31 -0600

    NO MORE CRACO

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Assuming you have a CSS file for global styles
 import App from './App'; // Import the main App component
-import config from './aws-exports';
 
 
 // Render the App component to the DOM
